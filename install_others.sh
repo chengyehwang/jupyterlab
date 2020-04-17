7dbfea7e82fa3e7c65bdb033b8940e9bb0c5237f
chengyehwang <cywang.eda@gmail.com>
2020-04-17 22:34:28 +0800

    fix

--- install_others.sh
+++ install_others.sh
@@ -1,4 +1,4 @@
 conda install -y openssl=1.1.1f
-#pip install aiohttp_swagger aiopg cufflinks jupyter-plotly-dash jupyterlab-sql simpy graphviz Dumper
+pip install aiohttp_swagger aiopg cufflinks jupyter-plotly-dash jupyterlab-sql simpy graphviz Dumper
 
-jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-dash@0.1.0-alpha.3
+jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings
