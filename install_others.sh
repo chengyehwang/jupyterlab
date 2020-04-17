f54dc82c7e12771fa72b1dd6196b4f0524949aa6
chengyehwang <cywang.eda@gmail.com>
2020-04-18 06:00:16 +0800

    remove old packages

--- install_others.sh
+++ install_others.sh
@@ -1,4 +1,4 @@
 conda install -y openssl=1.1.1f
-pip install aiohttp_swagger aiopg cufflinks jupyter-plotly-dash jupyterlab-sql simpy graphviz Dumper
+pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper
 
 jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings
