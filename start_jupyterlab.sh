89434ded8c776d9a198655d189ab1f390a104b3a
chengyehwang <cywang.eda@gmail.com>
2020-04-19 15:02:44 +0800

    suport run and control-c to stop

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -3,5 +3,5 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate
 host=`ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17.0`
 
-jupyter-lab --ip=$host --allow-root 2>&1 | sed "s@http://$host:8888@http://$1:8888@g"
+jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@http://$host:8888@http://$1:8888@g")
 
