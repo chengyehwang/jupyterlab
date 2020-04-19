84a470696c1687bf4db4c321b4f3d1b45dbc477d
chengyehwang <cywang.eda@gmail.com>
2020-04-19 15:12:25 +0800

    add server ip for check

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -3,5 +3,5 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate
 host=`ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17.0`
 
-jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@http://$host:8888@http://$1:8888@g")
+jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(.*\)   http://$host:8888\(.*\)@\1   http://$1:8888\2\n\1or http://$host:8888\2\n@g")
 
