3343db91acfd6f126869a1c974321a63ba58e71d
chengyehwang <cywang.eda@gmail.com>
2020-04-28 21:38:36 +0800

    remove xvfb

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -3,6 +3,5 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate
 host_ext=$1
 host=`ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17.0`
-Xvfb :1 &
 jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
 
