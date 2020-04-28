8b4805c154e5afe2a8e717bac5514e44c7fe2c43
chengyehwang <cywang.eda@gmail.com>
2020-04-29 07:02:05 +0800

    novnc

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -10,7 +10,7 @@ export DISPLAY=:1
 /usr/bin/Xvfb ${DISPLAY} -screen 0 1024x768x24 -noreset >& xvfb.log &
 /usr/bin/openbox >& openbox.log &
 x11vnc -forever -ncache 10 -display ${DISPLAY} -xkb -nopw -N >& vnc.log &
-/opt/novnc/utils/launch.sh --vnc localhost:5901 --listen 8880 >& novnc.log &
+/opt/novnc/utils/launch.sh --vnc ${host_ext}:5901 --listen 8880 >& novnc.log &
 
 jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
 
