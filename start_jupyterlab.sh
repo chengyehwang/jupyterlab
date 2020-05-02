46c6711ea3e91b4a44dd5a7de3d2f7f5984b7c61
chengyehwang <cywang.eda@gmail.com>
2020-05-02 21:18:52 +0800

    add code for setting

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -14,6 +14,6 @@ x11vnc -forever -ncache 10 -display ${DISPLAY} -xkb -nopw -N >& vnc.log &
 /opt/novnc/utils/launch.sh --vnc localhost:590${port} --listen 6080 >& novnc.log &
 
 echo "novnc access http://${host_ext}:6080/vnc.html"
-
+cp -rf user-settings ~/.jupyter/lab/
 jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
 
