e35c77ebd7f942e6a78ec1498d153226e46a7f1e
chengyehwang <cywang.eda@gmail.com>
2020-05-02 20:50:12 +0800

    add port to sync

--- demo.py
+++ demo.py
@@ -12,31 +12,31 @@
 #     name: python3
 # ---
 
-# %%
+# %% Collapsed="false"
 # %load_ext snakeviz
 import requests
 
-# %% [markdown]
+# %% [markdown] Collapsed="false"
 # # Pandas table
 
-# %%
+# %% Collapsed="false"
 import pandas as pd
 
-# %%
+# %% Collapsed="false"
 data = pd.DataFrame({'a':[1,2,3],'b':[3,4,5]})
 display(data)
 
-# %% [markdown]
+# %% [markdown] Collapsed="false"
 # # Chart by Plotly
 
-# %%
+# %% Collapsed="false"
 import plotly.express as px
 
-# %%
+# %% Collapsed="false"
 px.scatter(data, x='a', y='b')
 
 
-# %%
+# %% Collapsed="false"
 import plotly.graph_objects as go
 fig = go.Figure()
 fig.add_trace(go.Scatter(
@@ -45,68 +45,68 @@ fig.add_trace(go.Scatter(
 ))
 fig.show()
 
-# %% [markdown]
+# %% [markdown] Collapsed="false"
 # # Show image
 
-# %%
+# %% Collapsed="false"
 import plotly.io as pio
 from IPython.display import SVG, display, Image
 img_bytes = pio.to_image(fig, format="svg")
 display(SVG(img_bytes))
 
-# %%
+# %% Collapsed="false"
 from export_image import fig_to_png
 fig_to_png(fig,'out.png')
 Image(filename='out.png')
 
-# %% [markdown]
+# %% [markdown] Collapsed="false"
 # # SQL access
 
-# %%
+# %% Collapsed="false"
 # python database api specification v2.0
 
-# %%
+# %% Collapsed="false"
 import sqlite3
 import os
 
-# %%
+# %% Collapsed="false"
 if os.path.exists('test.db'):
     os.remove('test.db')
 
-# %%
+# %% Collapsed="false"
 cur = sqlite3.connect('test.db').cursor()
 
-# %%
+# %% Collapsed="false"
 cur.executescript('''
 CREATE TABLE EMPLOYEE(firstname varchar(50),lastname varchar(50));
 INSERT INTO EMPLOYEE VALUES('Tom','Mitchell');
 INSERT INTO EMPLOYEE VALUES('Jack','Ryan');
 ''');
 
-# %%
+# %% Collapsed="false"
 cur.execute("SELECT * FROM EMPLOYEE")
 out = cur.fetchall()
 display(out)
 
-# %% [markdown]
+# %% [markdown] Collapsed="false"
 # # Cython integration
 
-# %%
+# %% Collapsed="false"
 import pyximport
 pyximport.install(pyimport=True, language_level=3)
 
-# %%
+# %% Collapsed="false"
 import demo_cython
 
-# %%
+# %% Collapsed="false"
 out = demo_cython.func(1,2)
 display(out)
 
-# %% [markdown]
+# %% [markdown] Collapsed="false"
 # # Speed profiling
 
-# %%
+# %% Collapsed="false"
 # %%snakeviz
 data['t']=12
 
-# %%
+# %% Collapsed="false"
