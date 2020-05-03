cfb61bd590baa40fda52ce309b8a261c7e2b9e08
chengyehwang <cywang.eda@gmail.com>
2020-05-04 07:22:05 +0800

    clean

--- install_novnc.sh
+++ install_novnc.sh
@@ -6,7 +6,4 @@ jupyter labextension uninstall nbdime-jupyterlab
 apt-get install -y ca-certificates openssl xvfb x11vnc openbox supervisor
 git clone https://github.com/novnc/noVNC.git /opt/novnc
 git clone https://github.com/novnc/websockify.git /opt/novnc/utils/websockify
-#git clone https://github.com/chengyehwang/renku-jupyterlab-vnc
-#(cd renku-jupyterlab-vnc ; rm yarn.lock)
-#(cd renku-jupyterlab-vnc ; npm install ; npm run build ; jupyter labextension link .)
-#(cd renku-jupyterlab-vnc ; jupyter lab build)
+
--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -14,10 +14,6 @@ sleep 1
 x11vnc -forever -ncache 10 -listen localhost -display ${DISPLAY} -xkb -nopw -N >& vnc.log &
 xhost +
 cp /jupyterlab/jupyter_notebook_config.py /root/miniconda3/etc/jupyter/
-#/opt/novnc/utils/launch.sh --vnc localhost:590${port} --listen 6080 >& novnc.log &
 
-#echo "novnc access http://${host_ext}:6080/vnc.html"
-#cp -rf user-settings ~/.jupyter/lab/
-#/usr/local/bin/start-notebook.d/start-supervisord.sh
 jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
 
