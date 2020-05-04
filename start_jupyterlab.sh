789b8f415b320cfcae3517893fe8b986eb477b6f
chengyehwang <cywang.eda@gmail.com>
2020-05-04 20:14:56 +0800

    add index.html in novnc and redirect to vnc.html

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -14,6 +14,7 @@ sleep 1
 x11vnc -forever -ncache 10 -listen localhost -display ${DISPLAY} -xkb -nopw -N >& vnc.log &
 xhost +
 cp /jupyterlab/jupyter_notebook_config.py /root/miniconda3/etc/jupyter/
+cp /jupyterlab/vnc_index.html /opt/novnc/index.html
 
 jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
 
