9ec342819a03574aeb66e73890eaa5f1d42d26fa
chengyehwang <cywnag.eda@gmail.com>
2020-05-05 07:50:56 +0800

    back to jupyterlab 2.0, by new git and vim

--- install_others.sh
+++ install_others.sh
@@ -1,5 +1,5 @@
-conda install ptvsd openssl=1.1.1g xeus-python=0.6.7 notebook>=6 jupyterlab-git -y
+conda install ptvsd openssl=1.1.1g xeus-python notebook -y
 
-pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper
+pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git
 
-jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git
+jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim
