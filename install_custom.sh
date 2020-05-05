8c7a8c946017d955d82a1df577ac4db044a54178
chengyehwang <cywnag.eda@gmail.com>
2020-05-05 23:02:06 +0800

    use novnc only

--- install_custom.sh
+++ install_custom.sh
@@ -2,15 +2,6 @@ npm config set user 0
 npm config set unsafe-perm true
 npm install electron@7.2.2 -g
 
-apt-get install -y tightvncserver
-
-mkdir -p /root/.vnc
-echo "123456" | tightvncpasswd -f >  /root/.vnc/passwd
-chmod 700 /root/.vnc/passwd
-export USER=root
-vncserver 
-vncserver -kill :1
-echo "xhost +" >> /root/.vnc/xstartup
 apt install -y gdebi-core
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 gdebi -n google-chrome-stable_current_amd64.deb
--- install_others.sh
+++ install_others.sh
@@ -1,5 +1,7 @@
-conda install ptvsd openssl=1.1.1g xeus-python notebook -y
+conda install -y ptvsd openssl=1.1.1g xeus-python notebook jupyterlab_code_formatter
 pip install --upgrade pip
 pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git
 
-jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim
+jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim @ryantam626/jupyterlab_code_formatter
+
+jupyter serverextension enable --py jupyterlab_code_formatter
