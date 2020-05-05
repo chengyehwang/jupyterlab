926fa98e9ea692ae788fad027012dd010a80be3c
chengyehwang <cywnag.eda@gmail.com>
2020-05-05 08:13:31 +0800

    update pip

--- install_others.sh
+++ install_others.sh
@@ -1,5 +1,5 @@
 conda install ptvsd openssl=1.1.1g xeus-python notebook -y
-
+pip install --upgrade pip
 pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git
 
 jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim
