189b2118fc7cbb2d96871deb4ff7819ec36a2967
chengyehwang <cywnag.eda@gmail.com>
2020-05-06 05:20:59 +0800

    clean

--- install_others.sh
+++ install_others.sh
@@ -1,6 +1,6 @@
 conda install -y ptvsd openssl=1.1.1g xeus-python notebook jupyterlab_code_formatter black isort
-pip install --upgrade pip
-pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git
+
+pip install -y aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git
 
 jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim @ryantam626/jupyterlab_code_formatter
 
