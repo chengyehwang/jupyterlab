a1a7b47d4b2f99127d33d6512b1113f982e481ea
chengyehwang <cywang.eda@gmail.com>
2020-05-09 09:20:29 +0800

    parallel computing

--- demo_dask.py
+++ demo_dask.py
@@ -15,6 +15,7 @@
 import dask.bag as db
 import dask
 import time
+import os
 
 # # map
 
@@ -25,35 +26,63 @@ b = db.read_text('data/*.json')
 out = b.compute()
 print(len(out))
 
+# +
 sum = 0
+
+
 def test(*args):
     global sum
-    sum +=1
+    sum += 1
+
+
 c = db.read_text('data/*.json').map(test)
+# -
 
 c.compute();
 
-
 # # data flow
 
+from dask.distributed import Client
+client = Client()
+
+# +
+import math
 @dask.delayed
 def inc(i):
-    time.sleep(10)
-    return i + 1
+    sum = i
+    for x in range(100000000):
+        sum = sum * math.sin(sum)
+    return sum
+
+
 @dask.delayed
 def add(a, b):
-    time.sleep(10)
-    return a + b
+    sum = a + b
+    for x in range(100000000):
+        sum = sum * math.sin(sum)
+    return sum
 
+
+# -
 
 x1 = 1
 x2 = 2
 x3 = inc(x1)
 x4 = inc(x2)
-x5 = add(x3,x4)
+x5 = add(x3, x4)
 
 x5.visualize()
 
+
 # %time x5.compute()
+
+def run_job(cmd, *args):
+    if (False):  # md5 pass
+        pass
+    else:  # really need
+        print(os.popen(cmd).read())
+
+
+run_job('ls')
 
 
