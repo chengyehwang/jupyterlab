1c6d2d037e5c00d0bbcae6a200aa5bdb427eeb4f
chengyehwang <cywang.eda@gmail.com>
2020-05-09 20:09:48 +0800

    fix

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,8 +2,7 @@ c.ServerProxy.servers = {
         'novnc' : {
             'command': ['/opt/novnc/utils/launch.sh', '--vnc', 'localhost:5901', '--listen', '{port}', '>&', '/tmp/novnc.log', '&'],
             'port': 6080
-            }
-        }
+            },
         'prefect' : {
                 'command': ['prefect', 'server', 'start', '>&', '/tmp/prefect.log', '&'],
             'port': 8080
