412cea14b93bea104a7a66a053b1d8802b020136
chengyehwang <cywang.eda@gmail.com>
2020-05-14 04:18:23 +0800

    sync

--- install_novnc.sh
+++ install_novnc.sh
@@ -10,6 +10,6 @@ apt-get install -y ca-certificates openssl xvfb x11vnc openbox supervisor
 git clone https://github.com/novnc/noVNC.git /opt/novnc
 git clone https://github.com/novnc/websockify.git /opt/novnc/utils/websockify
 else
-apt-get install -y ca-certificates openssl xvfb
+apt-get install -y ca-certificates openssl xvfb supervisor
 fi
 
