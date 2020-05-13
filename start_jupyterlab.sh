75d7d8874f25cdb02d50412eb2efbda776d675b7
chengyehwang <cywang.eda@gmail.com>
2020-05-14 05:59:44 +0800

    add lib for x11 baseline

--- start_jupyterlab.sh
+++ start_jupyterlab.sh
@@ -5,10 +5,10 @@ host_ext=$1
 host=`ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17.0`
 port=1
 export SHELL=/bin/bash
-export DISPLAY=:${port}
+#export DISPLAY=:${port}
 
 # framebuffer only
-/usr/bin/Xvfb ${DISPLAY} -screen 0 1920x1080x24 -noreset >& xvfb.log &
+#/usr/bin/Xvfb ${DISPLAY} -screen 0 1920x1080x24 -noreset >& xvfb.log &
 
 
 # jupyterlab user setting
@@ -19,5 +19,5 @@ cp -rf /jupyterlab/user-settings ~/.jupyter/lab/
 ./start_minio.sh
 
 export NODE_OPTIONS=--max-old-space-size=4096
-jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
+xvfb-run -a jupyter-lab --ip=$host --allow-root --no-browser 2>&1 | (trap '' INT ; exec sed -u "s@\(\s*\)http://$host:8888\(.*\)@\1http://$host_ext:8888\2\n\1or http://$host:8888\2\n@g")
 
