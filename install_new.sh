c321f0e3977c9678fa9e29a6c7c33c4c65af41d2
chengyehwang <cywang.eda@gmail.com>
2020-05-19 23:46:55 +0800

    fix 20.04 focal

--- install_new.sh
+++ install_new.sh
@@ -1,6 +1,6 @@
-apt-get install -y gnupg
+apt-get install -y gnupg software-properties-common
 wget -q http://xpra.org/gpg.asc -O- | sudo apt-key add -
-add-apt-repository "deb https://xpra.org/ xenial main"
+add-apt-repository "deb https://xpra.org/ focal main"
 apt-get update
 apt-get install -y xpra xserver-xorg-video-dummy
 sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
