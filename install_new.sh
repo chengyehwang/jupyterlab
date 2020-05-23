d4f12fc41e5f1eb516e49841b1432c68e479d3be
chengyehwang <cywang.eda@gmail.com>
2020-05-23 11:06:10 +0800

    revert

--- install_new.sh
+++ install_new.sh
@@ -2,11 +2,7 @@ apt-get install -y gnupg software-properties-common
 wget -q http://xpra.org/gpg.asc -O- | sudo apt-key add -
 add-apt-repository "deb https://xpra.org/ focal main"
 apt-get update
-apt-get install -y xserver-xorg-video-dummy
+apt-get install -y xpra xserver-xorg-video-dummy
 sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
+wget https://xpra.org/xorg.conf
 
-apt-get install -y libx11-dev libxtst-dev libxcomposite-dev \
-    libxdamage-dev libxkbfile-dev xauth x11-xkb-utils xserver-xorg-video-dummy \
-    python-all-dev python-gobject-dev python-gtk2-dev cython \
-    libx264-dev libvpx-dev node-uglify yui-compressor
-pip install xpra
