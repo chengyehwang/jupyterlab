0fbbb94db6f39dd04a32977d8abc8ed1c2d71d00
chengyehwang <cywang.eda@gmail.com>
2020-06-14 11:22:29 +0000

    add imageai: image to object info

--- install_conda_package.sh
+++ install_conda_package.sh
@@ -1,7 +1,7 @@
-for i in conda-forge anaconda mutirri temporary-recipes anaconda-platform plotly
+for i in conda-forge anaconda mutirri temporary-recipes anaconda-platform plotly powerai
 do
     conda config --add channels $i
 done
 conda config --set channel_priority false
 conda update -n base -c defaults conda
-conda install -y conda python=3.7.* numpy lxml jupyter matplotlib pandas ipython future Pillow cython graphviz pygraphviz networkx psutil requests xlrd scipy scikit-learn parse jupyterlab=2.0.* jupyterlab-dash nodejs openssl electron tesseract jupytext plotly plotly-orca libgfortran-ng yarn python-pptx autopep8 snakeviz fs.sshfs patchelf openpyxl=3.0.0 tensorflow sqlite ipython-sql postgresql psycopg2 aiohttp pgspecial aiohttp ccache seaborn jsonlines conda-build conda-verify constructor pymongo sympy
+conda install -y conda python=3.7.* numpy lxml jupyter matplotlib pandas ipython future Pillow cython graphviz pygraphviz networkx psutil requests xlrd scipy scikit-learn parse jupyterlab=2.0.* jupyterlab-dash nodejs openssl electron tesseract jupytext plotly plotly-orca libgfortran-ng yarn python-pptx autopep8 snakeviz fs.sshfs patchelf openpyxl=3.0.0 tensorflow keras imageai opencv sqlite ipython-sql postgresql psycopg2 aiohttp pgspecial aiohttp ccache seaborn jsonlines conda-build conda-verify constructor pymongo sympy
