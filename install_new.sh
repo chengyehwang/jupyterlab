22558f779d4fdaa1807a9507fdbe5c4be8e2b6a6
chengyehwang <cywang.eda@gmail.com>
2020-06-15 06:51:02 +0800

    fix interactive

--- install_new.sh
+++ install_new.sh
@@ -2,7 +2,7 @@ apt-get install -y gnupg software-properties-common
 wget -q http://xpra.org/gpg.asc -O- | sudo apt-key add -
 add-apt-repository "deb https://xpra.org/ bionic main"
 apt-get update
-apt-get install -y xpra xserver-xorg-video-dummy
+export DEBIAN_FRONTEND=noninteractive && apt-get install -y xpra xserver-xorg-video-dummy
 sed -i 's/console/anybody/g' /etc/X11/Xwrapper.config
 wget https://xpra.org/xorg.conf
 ## android test auto
