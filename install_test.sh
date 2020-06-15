326863b2474443361eadf2ddc2673ce024ac6582
chengyehwang <cywang.eda@gmail.com>
2020-06-16 07:48:55 +0800

    upgrade python 3.8

--- install_test.sh
+++ install_test.sh
@@ -1,3 +1,5 @@
 # fix for cv2
-conda install -y gnutls=3.6.5 tensorflow=1.13.*
-pip install pgen kivy kivy_examples ffpyplayer SerpentAI
+conda install -y gnutls=3.6.5 tensorflow=1.13.* kivy ffpyplayer
+#pip install SerpentAI
+git clone https://github.com/SerpentAI/SerpentAI
+cd SerpertAI ; pip install .
