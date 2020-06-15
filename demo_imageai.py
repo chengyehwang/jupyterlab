aaf90a47519269573852b0ffea5b007caedbb39c
chengyehwang <cywang.eda@gmail.com>
2020-06-15 11:44:10 +0000

    run imageai

---
+++ demo_imageai.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import os
+if not os.path.exists('detection_config.json'):
+    os.system('wget https://github.com/OlafenwaMoses/ImageAI/releases/download/essential-v4/detection_config.json')
+
+# +
+from imageai.Detection.Custom import CustomObjectDetection
+
+detector = CustomObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath("hololens-ex-60--loss-2.76.h5")
+detector.setJsonPath("detection_config.json")
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image="holo2.jpg", output_image_path="holo2-detected.jpg")
+for detection in detections:
+    print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
+
+
+# -
+
+
