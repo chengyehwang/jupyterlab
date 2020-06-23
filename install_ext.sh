34ab6e96a47ddf3bbca5e319a0326fc91671ea6a
chengyehwang <cywang.eda@gmail.com>
2020-06-23 20:21:33 +0000

    install vaex

--- install_ext.sh
+++ install_ext.sh
@@ -4,7 +4,7 @@ conda install -y ptvsd openssl=1.1.1g xeus-python notebook jupyterlab_code_forma
 
 pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git jupyter-lsp
 
-jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim @ryantam626/jupyterlab_code_formatter qgrid2 @jupyterlab/server-proxy @krassowski/jupyterlab-lsp @bokeh/jupyter_bokeh
+jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim @ryantam626/jupyterlab_code_formatter qgrid2 @jupyterlab/server-proxy @krassowski/jupyterlab-lsp @bokeh/jupyter_bokeh jupyter-vue jupyter-leaflet
 
 jupyter serverextension enable --sys-prefix --py jupyterlab_code_formatter
 jupyter serverextension enable --sys-prefix jupyter_server_proxy
--- install_test.sh
+++ install_test.sh
@@ -3,3 +3,5 @@ conda install -y gnutls=3.6.5 tensorflow=1.13.* kivy ffpyplayer vaex
 #pip install SerpentAI
 #git clone https://github.com/SerpentAI/SerpentAI
 #cd SerpertAI ; pip install .
+#export NODE_OPTIONS=--max-old-space-size=4096
+#jupyter lab build
