c4bbea02bbdf9bb6eb0d885c8dddec86edc3c735
chengyehwang <cywang.eda@gmail.com>
2020-06-29 07:41:04 +0800

    port

--- gym-android/gym_android/envs/android_env.py
+++ gym-android/gym_android/envs/android_env.py
@@ -1,16 +1,32 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
 class AndroidEnv(gym.Env):
     metadata = {'render.modes':['human']}
-    def __init__(self):
-        pass
+    def __init__(self, verbose=1):
+        self.golden = [1,2,3,4]
+        self.state = [1,1,1,1]
+        self.count = 0
     def step(self, action):
-        pass
+        assert(len(action)>=4)
+        self.state[0] += action[0]
+        self.state[1] += action[1]
+        self.state[2] += action[2]
+        self.state[3] += action[3]
+        self.count += 1
+        step_reward = abs(np.sum(np.array(self.golden) - np.array(self.state)))
+        if step_reward < 1:
+            done = True
+        else:
+            done = False
+        return self.state, step_reward, done, {}
     def reset(self):
-        pass
+        self.count = 0
+        return
     def render(self, mode='human'):
-        pass
+        print(self.state)
+        return self.count < 1000
     def close(self):
-        pass
+        return
 
