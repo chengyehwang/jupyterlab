1d1b6741de2210f5fc330b9efb680119ca24cfac
cywang <cywang.eda@gmail.com>
2021-01-30 05:24:16 +0800

    fix

--- install_ios.sh
+++ install_ios.sh
@@ -3,5 +3,7 @@ export PATH=$PATH:/opt/homebrew/bin
 brew install autoconf automake libtool openssl pkg-config
 brew tap homebrew/cask-versions
 brew install --cask homebrew/cask-versions/adoptopenjdk8
+brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
 
-pip install python-for-android
+pip install kivy-ios
+
