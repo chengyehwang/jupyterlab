40908fb59777acb2e053f9e1b88ada99631c6677
chengyehwang <cywang.eda@gmail.com>
2021-01-29 23:51:46 +0800

    fix

--- install_ext.sh
+++ install_ext.sh
@@ -1,8 +1,12 @@
 export NODE_OPTIONS=--max-old-space-size=4096
-#conda install -y ptvsd openssl=1.1.1g xeus-python notebook jupyterlab_code_formatter black isort jupyter-server-proxy python-language-server ipympl bokeh ipyvolume=0.5.2
+conda install -y xeus-python notebook jupyterlab_code_formatter black isort python-language-server ipympl bokeh ipyvolume
 
 
 pip install aiohttp_swagger aiopg cufflinks simpy graphviz Dumper jupyterlab-git jupyter-lsp
+
+jupyter labextension install jupyterlab-plotly@4.14.3
+jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.3
+
 
 #jupyter labextension install jupyterlab-s3-browser jupyterlab-plotly @jupyterlab/toc jupyterlab-jupytext @jupyter-widgets/jupyterlab-manager jupyter-matplotlib @lckr/jupyterlab_variableinspector @aquirdturtle/collapsible_headings jupyterlab-spreadsheet jupyterlab-drawio @jupyterlab/debugger @jupyterlab/git @axlair/jupyterlab_vim @ryantam626/jupyterlab_code_formatter qgrid2 @jupyterlab/server-proxy @krassowski/jupyterlab-lsp @bokeh/jupyter_bokeh jupyter-vue jupyter-leaflet
 
