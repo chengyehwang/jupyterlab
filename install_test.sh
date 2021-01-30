7985c3118968b9eba349093f0941e54682b68561
chengyehwang <cywang.eda@gmail.com>
2021-01-31 07:58:52 +0800

    fix install

--- install_test.sh
+++ install_test.sh
@@ -7,5 +7,5 @@ conda install -y gnutls tensorflow kivy ffpyplayer vaex
 #jupyter lab build
 conda install -c akode -y gym
 pip install buildozer
-apt install -y zip unzip openjdk-8-jdk python3-pip autoconf libtool autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev
+apt install -y zip unzip openjdk-8-jdk python3-pip autoconf libtool autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev xclip xsel
 
