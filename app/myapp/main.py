80d501bf5728f2457ee4fe3f270c4ea62cdc87d1
chengyehwang <cywang.eda@gmail.com>
2021-01-31 22:00:33 +0000

    exec

--- app/myapp/main.py
+++ app/myapp/main.py
@@ -47,7 +47,15 @@ Builder.load_string("""
     BoxLayout:
         orientation:'vertical'
         Label:
-            text: 'Under Construction'
+            text: 'Python Interpreter'
+        TextInput:
+            id: input
+        Button:
+            text: 'Run'
+            on_press: root.run()
+        TextInput:
+            id: output
+            text: ''
         Button:
             text: 'Back to Top'
             on_press: root.manager.current = 'Top'
@@ -61,7 +69,34 @@ class EngineeringScreen(Screen):
     pass
 class ScienceScreen(Screen):
     pass
+
+import sys
+from io import StringIO
+import contextlib
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
 class DevelopScreen(Screen):
+    def run(self):
+        input = self.ids.input.text
+        print('============== input  ===========')
+        print(input)
+        with stdoutIO() as s:
+            try:
+                exec(input)
+            except:
+                print('error in code')
+        output = s.getvalue()
+        print('============== output ===========')
+        print(output)
+        self.ids.output.text = output
     pass
 
 class myApp(App):
@@ -72,6 +107,7 @@ class myApp(App):
         sm.add_widget(EngineeringScreen(name='Engineering'))
         sm.add_widget(ScienceScreen(name='Science'))
         sm.add_widget(DevelopScreen(name='Develop'))
+        
         return sm
 
 if __name__ == '__main__':
